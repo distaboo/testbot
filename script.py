2b000fd6a85c428cf5d01454ed04b55715baa5b3
Denis Gryaznov <distaboo@mail.ru>
2018-11-10 21:28:59 +0600

    1
    Signed-off-by: Denis Gryaznov <distaboo@mail.ru>

--- script.py
+++ script.py
@@ -12,14 +12,23 @@ markup_menu.add(btn_adress,btn_payment,btn_delivery)
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(func = lambda message:True,commands=['start', 'help'])
 def command_handler(message: Message):
     bot.reply_to(message, 'Hello', reply_markup=markup_menu)
 
-@bot.edited_message_handler(content_types=['text'])
-@bot.message_handler(content_types=['text'])
+@bot.edited_message_handler(func = lambda message:True,content_types=['text'])
+@bot.message_handler(func = lambda message:True,content_types=['text'])
 def echo_digits(message: Message):
-        bot.reply_to(message, 'Alex is good kid')
+        if message.text == 'Способы доставки':
+            bot.reply_to(message, 'Доставка курьером, самовывоз',reply_markup = markup_menu)
+        elif message.text == 'Способы оплаты':
+            bot.reply_to(message, 'Nal, beznal', reply_markup=markup_menu)
+        else:
+            bot.reply_to(message, 'Alex is good kid',reply_markup = markup_menu)
         return
-
+@bot.message_handler(func = lambda message:True,content_types=['location'])
+def location(message: Message):
+    lon = message.location.longitude
+    lat = message.location.latititude
+    print('Широта: {} Долгота: {}'.format(lon,lat))
 bot.polling(timeout=60)

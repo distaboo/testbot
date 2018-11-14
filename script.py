18b9b77fd700b57d863106a075ba255cf57f46a9
Denis Gryaznov <distaboo@mail.ru>
2018-11-14 15:32:15 +0600

    1
    Signed-off-by: Denis Gryaznov <distaboo@mail.ru>

--- script.py
+++ script.py
@@ -5,15 +5,20 @@ from telebot.types import Message
 
 TOKEN = "751120059:AAGGNfinmmdqDNYLyL2hP-FeeiMnCF3vp3E"
 markup_menu = types.ReplyKeyboardMarkup(resize_keyboard = True,row_width = 1)
-btn_adress = types.KeyboardButton('Список магазинов',request_location = True)
-btn_payment = types.KeyboardButton('Способы оплаты')
-btn_delivery = types.KeyboardButton('Способы доставки')
+btn_adress = types.KeyboardButton('Адреса точек продаж')
+btn_payment = types.KeyboardButton('Наши елки')
+btn_delivery = types.KeyboardButton('Связаться с нами')
 markup_menu.add(btn_adress,btn_payment,btn_delivery)
 
-markup_inline = types.InlineKeyboardMarkup()
-btn_cash = types.InlineKeyboardButton('Nalichnye', callback_data='cash')
-btn_card = types.InlineKeyboardButton('Beznal', callback_data='card')
-markup_inline.add(btn_card,btn_cash)
+contact_inline = types.InlineKeyboardMarkup()
+btn_call = types.InlineKeyboardButton('Позвонить', request_contact=True,callback_data='cash')
+btn_message = types.InlineKeyboardButton('Написать', callback_data='card')
+btn_number = types.InlineKeyboardButton('Оставить номер', callback_data='card')
+contact_inline.add(btn_call,btn_message,btn_number)
+
+
+
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func = lambda message:True,commands=['start', 'help'])
@@ -25,16 +30,20 @@ def command_handler(message: Message):
 def echo_digits(message: Message):
         if message.text == 'Способы доставки':
             bot.reply_to(message, 'Доставка курьером, самовывоз',reply_markup = markup_menu)
-        elif message.text == 'Способы оплаты':
-            bot.reply_to(message, 'Следующие способы оплаты', reply_markup=markup_inline)
-        else:
-            bot.reply_to(message, 'Alex is good kid',reply_markup = markup_menu)
+        elif message.text == 'Связаться с нами':
+            bot.reply_to(message, 'Позвоните нам или напишите в телеграм. '
+                                  'Также вы можете оставить нам свой номер и наш менеджер сам с вами свяжется',
+                         reply_markup=contact_inline)
         return
 @bot.message_handler(func = lambda message:True,content_types=['location'])
 def location(message: Message):
     lon = message.location.longitude
     lat = message.location.latititude
     print('Широта: {} Долгота: {}'.format(lon,lat))
+
+@bot.message_handler(func = lambda message:True,content_types=['contact'])
+def location(message: Message):
+    bot.send_message(message.chat.id, text=message.contact)
 
 @bot.callback_query_handler(func = lambda call:True)
 def call_back_pay(call):

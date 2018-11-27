c021d75402e0138b6bb2d33879e39714113d8cdb
Denis Gryaznov <distaboo@mail.ru>
2018-11-27 13:20:55 +0600

    1
    Signed-off-by: Denis Gryaznov <distaboo@mail.ru>

--- script.py
+++ script.py
@@ -4,24 +4,17 @@ import telebot
 from telebot.types import Message
 
 TOKEN = "751120059:AAGGNfinmmdqDNYLyL2hP-FeeiMnCF3vp3E"
-markup_menu = types.ReplyKeyboardMarkup(resize_keyboard = True,row_width = 1)
-btn_adress = types.KeyboardButton('Адреса точек продаж')
-btn_payment = types.KeyboardButton('Наши елки',request_location=True)
-btn_delivery = types.KeyboardButton('Связаться с нами')
-
-markup_menu.add(btn_adress,btn_payment,btn_delivery)
-
-contact_inline = types.InlineKeyboardMarkup()
-btn_call = types.InlineKeyboardButton('Позвонить', callback_data='cash')
-btn_message = types.InlineKeyboardButton('Написать', callback_data='card')
-btn_number = types.InlineKeyboardButton('Оставить номер', callback_data='contact')
-contact_inline.add(btn_call,btn_message,btn_number)
-
-kb = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-kbtn1 = types.KeyboardButton("??")
-kbtn2 = types.KeyboardButton("?")
-
 bot = telebot.TeleBot(TOKEN)
+# or add KeyboardButton one row at a time:
+markup = types.ReplyKeyboardMarkup()
+itembtna = types.KeyboardButton('a')
+itembtnv = types.KeyboardButton('v')
+itembtnc = types.KeyboardButton('c')
+itembtnd = types.KeyboardButton('d')
+itembtne = types.KeyboardButton('e')
+markup.row(itembtna, itembtnv)
+markup.row(itembtnc, itembtnd, itembtne)
+
 
 @bot.message_handler(func = lambda message:True,commands=['start', 'help'])
 def command_handler(message: Message):
@@ -30,31 +23,8 @@ def command_handler(message: Message):
 @bot.edited_message_handler(func = lambda message:True,content_types=['text'])
 @bot.message_handler(func = lambda message:True,content_types=['text'])
 def echo_digits(message: Message):
-        if message.text == 'Способы доставки':
-            bot.reply_to(message, 'Доставка курьером, самовывоз',reply_markup = kb)
-        elif message.text == 'Адреса точек продаж':
-            bot.reply_to(message, 'Позвоните нам или напишите в телеграм. '
-                                  'Также вы можете оставить нам свой номер и наш менеджер сам с вами свяжется',
-                         reply_markup=kb)
+        bot.send_message(message.chat_id, "Choose one letter:", reply_markup=markup)
 
         return
-@bot.message_handler(func = lambda message:True,content_types=['location'])
-def location(message: Message):
-    lon = message.location.longitude
-    lat = message.location.latititude
-    print('Широта: {} Долгота: {}'.format(lon,lat))
-
-#@bot.message_handler(func = lambda message:True,content_types=['contact'])
-#def location(message: Message):
-#    bot.send_message(message.chat.id, text=message.contact)
-
-@bot.callback_query_handler(func = lambda call:True)
-def call_back_pay(call):
-    if call.data == 'contact':
-        bot.send_message(call.message.chat.id,text="kfkfkf")
-    if call.data == 'card':
-        bot.send_message(call.message.chat.id,text='Вы оплатили наличными')
-
-
 
 bot.polling(timeout=60)

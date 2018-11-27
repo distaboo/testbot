0eb3dd74e24816dd75c20dfb14321c3be42f9ecd
Denis Gryaznov <distaboo@mail.ru>
2018-11-27 16:34:17 +0600

    1
    Signed-off-by: Denis Gryaznov <distaboo@mail.ru>

--- script.py
+++ script.py
@@ -7,20 +7,25 @@ TOKEN = "751120059:AAGGNfinmmdqDNYLyL2hP-FeeiMnCF3vp3E"
 bot = telebot.TeleBot(TOKEN)
 # or add KeyboardButton one row at a time:
 markup = types.ReplyKeyboardMarkup()
-itembtna = types.KeyboardButton('a')
-itembtnv = types.KeyboardButton('v')
-itembtnc = types.KeyboardButton('c')
-itembtnd = types.KeyboardButton('d')
-itembtne = types.KeyboardButton('e')
-markup.row(itembtna, itembtnv)
-markup.row(itembtnc, itembtnd, itembtne)
+itembtna = types.KeyboardButton('Позвонить')
+itembtnv = types.KeyboardButton('Написать')
+itembtnc = types.KeyboardButton('Оставить номер')
+itembtnd = types.KeyboardButton('Вернуться в меню')
 
+markup.row(itembtna, itembtnv)
+markup.row(itembtnc, itembtnd)
+
+main_markup = types.ReplyKeyboardMarkup()
+itembtnPhoto = types.KeyboardButton('Позвонить')
+itembtnCall = types.KeyboardButton('Написать')
+itembtnAdress = types.KeyboardButton('Оставить номер')
+main_markup.add(itembtnAdress,itembtnPhoto,itembtnCall)
 
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    bot.send_message(message.chat.id, "hello", reply_markup=main_markup)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):

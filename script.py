335fb31c3690a321eb6dcbc38a7cb7215b52c29a
Denis Gryaznov <distaboo@mail.ru>
2018-11-28 22:04:33 +0600

    1
    Signed-off-by: Denis Gryaznov <distaboo@mail.ru>

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ from telebot.types import Message
 
 TOKEN = "751120059:AAGGNfinmmdqDNYLyL2hP-FeeiMnCF3vp3E"
 bot = telebot.TeleBot(TOKEN)
+admin_id = 406407068
 # or add KeyboardButton one row at a time:
 markup = types.ReplyKeyboardMarkup()
 itembtna = types.KeyboardButton('Позвонить')
@@ -44,6 +45,33 @@ def echo_all(message):
         bot.send_photo(message.chat.id, open('2.png', 'rb'))
     elif (message.text == 'Позвонить'):
         bot.send_contact(message.chat.id,+79231513104,'Алексей')
+    elif (message.text == 'Написать'):
+        bot.send_message(message.chat.id,
+                         "Напишите свое сообщение",
+                         reply_markup=main_markup)
+    else:
+        #bot.send_message(admin_id, settings.NEW_FEEDBACK)
+        keyboard = types.InlineKeyboardMarkup()
+        callback_button_yes = types.InlineKeyboardButton("Отправить", callback_data='yes')
+        callback_button_no = types.InlineKeyboardButton("Вернуться", callback_data='no')
+        keyboard.add(callback_button_yes, callback_button_no)
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    print("callback handler")
+    if call.message:
+        if call.data == "yes":
+            bot.forward_message(admin_id, call.message.chat.id, call.message.message_id - 2)
+            # TODO: find another way to forward message (without id-2)
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.message_id,
+                                  text="Отправлено!")
+
+        elif call.data == "no":
+            bot.send_message(admin_id, "@" + call.from_user.username + "Хотел написать, но не написал")
+            # TODO: user can be without USERNAME?
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.message_id,
+                                  text="(")
 
 bot.polling()

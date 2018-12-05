c707c3e460b83f47a95a82b7301c4019b2cf7873
Denis Gryaznov <distaboo@mail.ru>
2018-12-05 16:49:23 +0600

    1
    Signed-off-by: Denis Gryaznov <distaboo@mail.ru>

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ from bt import cafeBot
 
 bot1 = bt("751120059:AAGGNfinmmdqDNYLyL2hP-FeeiMnCF3vp3E",406407068)
 bot1.strat()
-bot2 = cafeBot("664435594:AAHUcl61psQuC5LvPqq7L-JZvMCmOX62SGU",406407068)
-bot2.strat()
+#bot2 = cafeBot("664435594:AAHUcl61psQuC5LvPqq7L-JZvMCmOX62SGU",406407068)
+#bot2.strat()
 
